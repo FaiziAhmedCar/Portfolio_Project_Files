7f6c04670e6751630c8d140f33e62816bd99e729
Faizi_Ahmed <125595034+FaiziAhmedCar@users.noreply.github.com>
2025-02-28 21:51:25 +0530

    css file added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className=' h-screen w-screen flex flex-col items-center justify-center' >
+    <h1 className='text-white'>Hello</h1> 
+    <h1 className="text-white">Gradient Text</h1>
+    </div>
+  )
+}
+
+export default App
