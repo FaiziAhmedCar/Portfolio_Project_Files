cf6cf542c053d8435d08c1e08e01ddb49fb713fc
Faizi_Ahmed <125595034+FaiziAhmedCar@users.noreply.github.com>
2025-03-21 00:54:47 +0530

    feat: add Clients section to the application

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import Navbar from "./sections/navbar.jsx";
 import Hero from "./sections/Hero.jsx";
 import About from "./sections/About.jsx";
 import Projects from "./sections/Projects";
+import Clients from "./sections/Clients.jsx";
 
 const App = () => {
   return (
-    <div className="max-w-7xl mx-auto  ">
+    <div className="max-w-7xl mx-auto "  >
       <Navbar />
       <Hero />
       <About />
       <Projects />
+      <Clients />
     </div>
   );
 };
